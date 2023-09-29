e1ef44acfa84ec811459eb6fb692c534326b0690
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2023-09-29 23:25:11 +0200

    Add files via upload

---
+++ geographyquiz.py
@@ -0,0 +1,42 @@
+print("Welcome to the geography quiz")
+
+answer = input("Are you ready to start? (yes/no): ")
+score = 0
+
+if answer.lower() == "yes":
+    answer = input("1. What is the capital of France? ")
+    if answer == 'Paris':
+        score += 1
+        print("Well done")
+    else:
+        print("Incorrect")
+
+    answer = input("2. What is the capital of Indonesia? ")
+    if answer == "Jakarta":
+        score += 1
+        print("Well done")
+    else:
+        print("Incorrect")
+
+    answer = input("3. What is the capital of Peru: ")
+    if answer == "Lima":
+        score += 1
+        print("Well done")
+    else:
+        print("Incorrect")
+
+    answer = input("4. What is the capital of Botswana: ")
+    if answer == "Gaborone":
+        score += 1
+        print("Well done")
+    else:
+        print("Incorrect")
+
+    answer = input("5. What is the capital of Spain: ")
+    if answer == "Madrid":
+        score += 1
+        print("Well done")
+    else:
+        print("Incorrect")
+
+print("Your total score is", score)
