87ca4f3e91f12c17b51ba35e92bad9e86cb4f674
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2023-10-01 23:49:33 +0200

    Update quizanimals.py

--- quizanimals.py
+++ quizanimals.py
@@ -11,21 +11,21 @@ print("Okay, let's play")
 answer = input("How many legs does a spider have? ")
 if answer.lower() == "eight" or answer == "8":
     print("Correct")
-    score += 1
+    score += 8
 else:
     print("Incorrect")
 
 answer = input("How many legs does a bee have? ")
 if answer.lower() == "six" or answer == "6":
     print("Correct")
-    score += 1
+    score += 6
 else:
     print("Incorrect")
 
 answer = input("How many legs does a cat have? ")
 if answer.lower() == "four" or answer == "4":
     print("Correct")
-    score += 1
+    score += 4
 else:
     print("Incorrect")
 
