e6a670d77eef6ae6af3daaa0461ea24505f6276a
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2023-10-03 22:29:18 +0200

    Update sys.py

--- sys.py
+++ sys.py
@@ -1,8 +1,11 @@
 import sys
 print(sys.stdin.readline())
-
 def joke():
-    if age >= 30 <= 50:
+    print("How old are you?")
+    age = int(sys.stdin.readline())
+    if age >= 30 and age <= 50:
         print("What is the result of 30 + 76 + 83 + 90 * 50?")
     else:
         print("What are you talking about?")
+
+joke()
