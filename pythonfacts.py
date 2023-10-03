6136bef176c0557b4495d3180b09f9346032f8d7
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2023-10-03 22:48:26 +0200

    Add files via upload

---
+++ pythonfacts.py
@@ -0,0 +1,5 @@
+import random
+
+python_info = ["With random.choice, you can pick a random item from a list",
+               "With shuffle, you can shuffle a list"]
+print(random.choice(python_info))
