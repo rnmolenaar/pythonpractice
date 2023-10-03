0d41eb0a0dc572390497515d81d831c0ed045193
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2023-10-03 22:52:36 +0200

    Update pythonfacts.py

--- pythonfacts.py
+++ pythonfacts.py
@@ -1,5 +1,6 @@
 import random
 
 python_info = ["With random.choice, you can pick a random item from a list",
-               "With shuffle, you can shuffle a list"]
+               "With shuffle, you can shuffle a list"
+              "if you want to know what version of Python you are using, use print("sys.version"]
 print(random.choice(python_info))
