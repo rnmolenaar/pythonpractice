cd998d04efd354dcd36bb0670046eb3a96f5d5cf
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2023-10-04 16:58:14 +0200

    Update pythonfacts.py

--- pythonfacts.py
+++ pythonfacts.py
@@ -1,8 +1,9 @@
 import random
 
 python_info = ["With random.choice, you can pick a random item from a list",
-               "With shuffle, you can shuffle a list"
-              "if you want to know what version of Python you are using, use print("sys.version")
-              "print(time.time()) to get local time in seconds after 1-1 1970"
+               "With shuffle, you can shuffle a list",
+              "if you want to know what version of Python you are using, use print("sys.version"),
+              "print(time.time()) to get local time in seconds after 1-1 1970",
+              "time.sleep()to put a pause in  for x in range(0, max): print(x)time.sleep(0.012)"
               ]
 print(random.choice(python_info))
