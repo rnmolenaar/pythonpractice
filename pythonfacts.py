c67a563ec0392455896f954c2e0b5b5f8ea613b9
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2023-10-05 00:00:59 +0200

    Update pythonfacts.py

--- pythonfacts.py
+++ pythonfacts.py
@@ -3,11 +3,12 @@ import random
 python_info = ["With random.choice(), you can pick a random item from a list",
                "With shuffle, you can shuffle a list",
               "if you want to know what version of Python you are using, use print("sys.version"),
-              "print(time.time()) to get local time in seconds after 1-1 1970",
+              "print(time.time()) to get local time in seconds after 1-1 1970. This is called the Epoch",
               "time.sleep()to put a pause in  for x in range(0, max): print(x)time.sleep(0.012)"
               "TensorFlow is a module related to machine learning" 
               "SciPy is a module related to algoritms for scientific computing"
-              "PyTorch is a library for irregular input data "
+              "PyTorch is a library for irregular input data."
+              
               
               ]
 print(random.choice(python_info))
