218f793719334214bfc4a5e8732da07814a91e05
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2023-10-04 16:57:00 +0200

    Update timepractice.py

--- timepractice.py
+++ timepractice.py
@@ -9,3 +9,21 @@ def counting_time(max):
     print("it took %s seconds" % (t2 - t1))
 
 counting_time(1718)
+
+
+# to put a pause in
+
+import time
+
+
+def counting_time(max):
+    t1 = time.time()
+    for x in range(0, max):
+        print(x)
+        time.sleep(0.012)
+    t2 = time.time()
+
+    print("it took %s seconds" % (t2 - t1))
+
+
+counting_time(171)
