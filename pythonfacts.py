094500f6911cb314dff7d265aed9e0dc11c2c1de
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2023-10-04 23:57:38 +0200

    Update pythonfacts.py

--- pythonfacts.py
+++ pythonfacts.py
@@ -5,5 +5,6 @@ python_info = ["With random.choice(), you can pick a random item from a list",
               "if you want to know what version of Python you are using, use print("sys.version"),
               "print(time.time()) to get local time in seconds after 1-1 1970",
               "time.sleep()to put a pause in  for x in range(0, max): print(x)time.sleep(0.012)"
+              "TensorFlow is a module related to machine learning" 
               ]
 print(random.choice(python_info))
