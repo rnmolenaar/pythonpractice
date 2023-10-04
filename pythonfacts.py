2b1144217be4ab6a640ef89bbfc7f2855e85ee8e
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2023-10-04 23:58:41 +0200

    Update pythonfacts.py

--- pythonfacts.py
+++ pythonfacts.py
@@ -6,5 +6,6 @@ python_info = ["With random.choice(), you can pick a random item from a list",
               "print(time.time()) to get local time in seconds after 1-1 1970",
               "time.sleep()to put a pause in  for x in range(0, max): print(x)time.sleep(0.012)"
               "TensorFlow is a module related to machine learning" 
+              "SciPy is a module related to algoritms for scientific computing"
               ]
 print(random.choice(python_info))
