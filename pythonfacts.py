2a32f9b88545749de8b886bd57665961320ccd15
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2023-10-04 17:00:00 +0200

    Update pythonfacts.py

--- pythonfacts.py
+++ pythonfacts.py
@@ -1,6 +1,6 @@
 import random
 
-python_info = ["With random.choice, you can pick a random item from a list",
+python_info = ["With random.choice(), you can pick a random item from a list",
                "With shuffle, you can shuffle a list",
               "if you want to know what version of Python you are using, use print("sys.version"),
               "print(time.time()) to get local time in seconds after 1-1 1970",
