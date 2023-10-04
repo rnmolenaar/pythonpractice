5aafe2686e954fadb32e9d91f36fbf8fcd8dfe4e
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2023-10-05 00:36:18 +0200

    Create todo.py

---
+++ todo.py
@@ -0,0 +1,8 @@
+import random
+
+to_do = ["read in book", "search online", "Udemy 10", "Udemy 20", "CTG", 
+         "Hang clothes", "Throw away", "put away", "Arms exercise", "Legs Exercise"
+         "Japanese word", "U word", "Knit", "Write", "Bid", "Udemy 30", 
+         "Charge phone", "Find something missing", "Pull straight",
+         "Drink water", "Check plants", "Card", "Clean Kitchen"]
+print(random.choice(to_do))
