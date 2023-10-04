22c9bdfa84816376673f58b2bb761e1fd1a54c7e
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2023-10-04 16:49:20 +0200

    Update timepractice.py

--- timepractice.py
+++ timepractice.py
@@ -1,1 +1,11 @@
-print("hello")
+import time
+
+def counting_time(max):
+    t1 = time.time()
+    for x in range(0, max):
+       print(x)
+    t2 =  time.time()
+
+    print("it took %s seconds" % (t2 - t1))
+
+counting_time(1718)
