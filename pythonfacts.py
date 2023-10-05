33b3c42ea635db6797f118be9b8984a90b4d597c
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2023-10-05 23:37:22 +0200

    Update pythonfacts.py

--- pythonfacts.py
+++ pythonfacts.py
@@ -6,10 +6,12 @@ python_info = ["With random.choice(listname), you can pick a random item from a 
               "print(time.time()) to get local time in seconds after 1-1 1970. This is called the Epoch",
               "time.sleep()to put a pause in  for x in range(0, max): print(x)time.sleep(0.012)"
               "TensorFlow is a module related to machine learning" 
-              "SciPy is a module related to algoritms for scientific computing"
+              "SciPy is a module related to algoritms for scientific computing,
               "PyTorch is a library for irregular input data."
               "class Skyscrapers(Buildings): here Skyscrapes is a child class of Buildings",
               "ntfy can be used to send SMS by phone", 
-              "MicroPython is being used to write Python code to control hardware. There is no C necessary"
+              "MicroPython is being used to write Python code to control hardware. There is no C necessary",
+              "NumPy is a library to work with numbers in Python",
+              
               }
   print(random.choice(python_info))
