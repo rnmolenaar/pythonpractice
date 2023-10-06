d933450ba537d87b386165be4e83d6dc505785b2
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2023-10-06 14:04:20 +0200

    Update todo.py

--- todo.py
+++ todo.py
@@ -4,5 +4,5 @@ to_do = ["read in book", "search online", "Udemy 10", "Udemy 20", "CTG",
          "Hang clothes", "Throw away", "put away", "Arms exercise", "Legs Exercise"
          "Japanese word", "U word", "Knit", "Write", "Bid", "Udemy 30", 
          "Charge phone", "Find something missing", "Pull straight",
-         "Drink water", "Check plants", "Card", "Clean Kitchen"]
+         "Drink water", "Check plants", "Card", "Clean Kitchen", "clap"]
 print(random.choice(to_do))
