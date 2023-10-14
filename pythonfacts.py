7bdebc865518dd69e8b8079adb357b1af0713c53
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2023-10-14 21:57:53 +0200

    Update pythonfacts.py

--- pythonfacts.py
+++ pythonfacts.py
@@ -36,6 +36,9 @@ python_info = ["With random.choice(listname), you can pick a random item from a 
                "// is integer division",
                "assign multiple variables at once: all, at, once = 2, 3, 4",
                "== operator to compare the values of two objects. The is operator compares the identity of two objects.",
+               "Dynamic typing= types can change easily in Python",
+               " 2 ** 3 ** 2 is hetzelfde als 2 ** (3 ** 2)",
+               
 
                
                
