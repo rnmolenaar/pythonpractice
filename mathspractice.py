942b6cc245ab731d80362e6832fac1692c06c7ea
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2023-10-18 01:05:09 +0200

    Update mathspractice.py

--- mathspractice.py
+++ mathspractice.py
@@ -3,3 +3,13 @@ for number in range(1, 101):
     square = number**6
     squares.append(square)
 print(squares)
+
+--
+from random import randint
+number = randint(1, 100) 
+
+if number % 2 != 0:
+    print("odd")
+else:
+    print("even")
+    
