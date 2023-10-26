494628e2df216da63c4a1e8f5f0a2fba58f87288
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2023-10-26 16:00:28 +0200

    Update loop.py

--- loop.py
+++ loop.py
@@ -6,7 +6,6 @@ while msg != "stop":
 print("Okay, chill")
 
 ###
-
 import random
 
 random_number = random.randint(1, 20)
@@ -21,4 +20,9 @@ while guess != random_number:
         print("It's too high")
     else :
         print("You guessed it")
-
+        continue_game = input("do you want to play again? (y/n) ")
+        if continue_game == "y":
+            random_number =random.randint(1,10)
+            guess = None
+        else:
+            print("see you next time")
