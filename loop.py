be0e7d79373d5a1f4a12173a131e320cd4e700e1
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2023-10-26 15:56:12 +0200

    Update loop.py

--- loop.py
+++ loop.py
@@ -4,3 +4,21 @@ while msg != "stop":
     print(msg)
     msg = input()
 print("Okay, chill")
+
+###
+
+import random
+
+random_number = random.randint(1, 20)
+guess = None
+
+while guess != random_number:
+    guess = input("Guess a number ")
+    guess = int(guess)
+    if guess < random_number:
+        print("It's too low")
+    elif guess > random_number:
+        print("It's too high")
+    else :
+        print("You guessed it")
+
