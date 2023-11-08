0e7a676e6598c66c59c40887b064884f96ecb7ea
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2023-11-08 16:30:38 +0100

    Update listcomprehension.py

--- listcomprehension.py
+++ listcomprehension.py
@@ -1,5 +1,12 @@
 nums = [1, 2, 3, 4]
 print([num*5 for num in nums])
 
-#next
+#List comprehension is more concise than doing a for loop
+
+
+nums = [1, 2, 3, 4]
+
+string_nums = [str(num) for num in nums]
+print(string_nums)
+
 
