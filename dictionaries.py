24fd082a6beea07534ddd2b28f01f29918cad22a
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2023-11-10 21:43:27 +0100

    Update dictionaries.py

--- dictionaries.py
+++ dictionaries.py
@@ -1,3 +1,12 @@
 
 #Make a dictionary
 character = {"name": "Smiley", "fan of": "River music", "Speaks": "A language"}
+
+# How to add values of different keys together in a dictionary
+
+person = {
+    "first": "Janie",
+    "last": "Grey",
+}
+
+full_name = person["first"] + " " + person["last"]
