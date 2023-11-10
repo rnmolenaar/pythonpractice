5a7bceca0da8009a11593a6dc43149638f90ff63
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2023-11-10 22:07:19 +0100

    Update dictionaries.py

--- dictionaries.py
+++ dictionaries.py
@@ -24,3 +24,21 @@ for value in dictionary.values():
 for k, v in dictionary.items():
     print(f:"key is {k} and value is {v}")
     
+# How to add all values in a dictionary
+
+gifts = dict(Lily=2.0, Mina=8.9, Pat=12.0, Lionel=9.5, Sile=15.0)
+
+total_gifts = 0
+
+for gift in gifts.values():
+    total_gifts += gift
+
+#or
+
+    total_gifts = sum(gift for gift in gifts.values())
+
+# or
+
+total_gifts = sum(gifts.values())
+
+    
