ac3e20621333c355fa27421d60cda672334450af
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2023-11-13 16:35:12 +0100

    Update pythonfacts.py

--- pythonfacts.py
+++ pythonfacts.py
@@ -26,8 +26,8 @@ python_info = ["With random.choice(listname), you can pick a random item from a 
                " list methods. count: counts how many times an element is in the list, extend: you can add more than one to a list",
                " list methods. index: returns the lowest index where the element appears, insert: adds element to the index number provided",
                " list methods. pop: removes and returns the last value or a given one",
-               "list method index: you can specify where in the list you want to look. list,index("word", 2, 5)
-               "list method: remove: you name the actual name of what is in the list and the first one of that is removed",
+               "list method index: you can specify where in the list you want to look. list,index('word', 2, 5)",
+             "list method: remove: you name the actual name of what is in the list and the first one of that is removed",
                "list method pop: if no index is specified, the last is removed",
                "list methods: reverse: changes the order from start to finish, sort: sorts the list.",
                "list methods: min: calculates the minimum of all elements in the list, calculates the max",
@@ -53,7 +53,7 @@ python_info = ["With random.choice(listname), you can pick a random item from a 
                "random.randint(0, 100) if only random is imported. If randint specifically, no need for random",
                "input(something with quotation marks ).lower()",
                "ranges used with for loops only it seems",
-               "list.index("Temenos") specifies what index Temenos has",
+               "list.index('Temenos') specifies what index Temenos has",
                "If you do range(7) it will give you the numbers 0-6",
                "anything you can do with a for look, you can do with a while loop, but not the other way around",
                "While loops run, while a condition is truthy",
@@ -65,52 +65,41 @@ python_info = ["With random.choice(listname), you can pick a random item from a 
                " ' '. join(listname) joins the words of listname into a string with what's before it as what is put between them",
                "Slicing. [] are used for this. There are three elements: list[start:end:step",
                " 'wordddddddds'[::2] = wrddddds] one gets removed constantly",
-               "swapping is possible in lists", 
+               "swapping is possible in lists",
                "You can create a new list from another list: [x*10 for x in nums].",
                "List comprehension: to create a new list based on the values of an existing list",
                "there is a boolean function: bool()",
-               " an empty string, empty list and 0 is falsy".
+               " an empty string, empty list and 0 is falsy",
                "You can use conditionals with list comprehension",
-              "answer = [flower[0] for flower in ["Rose", "Lily", "Primsose"]] list comprehension",
-              "answer = [letter for letter in 'partook' if letter not in 'aeiou'] to make a list with the consonants of that word ",              
-              "If you want to access a specific item in a nested list, do [9][1]",
-              "lists = [[l for l in range(0,30)] for num in range(0,30)] outcome [[0, 1, 2], [0, 1, 2], [0, 1, 2]] note the ]",
-              " .values() for dictionary. You can run a for loop on that to get all the info out",
-              "if you want to add the total of values in a dictionary: total_gifts = sum(gifts.values())",              
-              "dict: if you want to test for the existence of keys, use in. Values is in dictionary.values()",
-              "dict methods: clear empties the dict. copy makes a copy of the dict.".
-              "dict method: fromkeys {}.fromkeys() if you want to set every property to whatever you want it to. You can set a range",
-              "dict method: gets a key from a dict, but retrieves none instead of an error if it isn't there", 
-              "dict method: pop: you have to provide the key to remove key-value pair. popitem randomly removes something. No key needed", 
-              "dict method: update will add to another dictionary which can already have key-value pairs. Doesn't remove anything",
-              "to add a key value pair to a dictionary: dict['key_name'] = 'Value'",
-              "For dictionary comprehension syntax: {   : for .. in...}   ",
-              "parity refers to whether a number is odd or even",
-              "zip function is used to zip two dictionaries together into one",
-              "A list of pairs can be turned into a dictionary using dict() dictionary1 = dict(listname)",
-              "chr() function returns a string when entering ASCII code",
-              "tuple is immutable. tuple is faster",
-              "dynamically typed language is when the type of a variable is checked during run time",
-              "Tuples can be a key in dictionary. Like with coordinates of a location {(coord,coord)Amsterdam Zoo} Can't use a list as a key",
-              "Tuple method: count. the number of times a value is in a tuple. tuple.count("happy"). Index: where a value is found inside the tuple",
-              "tuples can be nested and you can also slice them",
-              "sets don't have order and no duplicate values. No index because no order {} are used. there is also a set function like dict function",
-              "set methods: add t.add(4) only works if the value isn't there yet. Remove. t.remove(2) 2 is removed.", 
-              "set method: discard is you don't want to worry about errors if something isn't there. It removes something", 
-              "set method: copy. copies. Clear: removes everything.",
-              "set math: | group two sets together. & look at those who are in both sets",
-              
-              
-              
-              
-              
-
-               
-               
-              
-               
-               
-               
+               "answer = [flower[0] for flower in ['Rose', 'Lily', 'Primsose']] list comprehension",
+               "answer = [letter for letter in 'partook' if letter not in 'aeiou'] to make a list with the consonants of that word ",
+               "If you want to access a specific item in a nested list, do [9][1]",
+               "lists = [[l for l in range(0,30)] for num in range(0,30)] outcome [[0, 1, 2], [0, 1, 2], [0, 1, 2]] note the ]",
+               " .values() for dictionary. You can run a for loop on that to get all the info out",
+               "if you want to add the total of values in a dictionary: total_gifts = sum(gifts.values())",
+               "dict: if you want to test for the existence of keys, use in. Values is in dictionary.values()",
+               "dict methods: clear empties the dict. copy makes a copy of the dict.",
+               "dict method: fromkeys {}.fromkeys() if you want to set every property to whatever you want it to. You can set a range",
+               "dict method: gets a key from a dict, but retrieves none instead of an error if it isn't there",
+               "dict method: pop: you have to provide the key to remove key-value pair. popitem randomly removes something. No key needed",
+               "dict method: update will add to another dictionary which can already have key-value pairs. Doesn't remove anything",
+               "to add a key value pair to a dictionary: dict['key_name'] = 'Value'",
+               "For dictionary comprehension syntax: {   : for .. in...}   ",
+               "parity refers to whether a number is odd or even",
+               "zip function is used to zip two dictionaries together into one",
+               "A list of pairs can be turned into a dictionary using dict() dictionary1 = dict(listname)",
+               "chr() function returns a string when entering ASCII code",
+               "tuple is immutable. tuple is faster",
+               "dynamically typed language is when the type of a variable is checked during run time",
+               "Tuples can be a key in dictionary. Like with coordinates of a location {(coord,coord)Amsterdam Zoo} Can't use a list as a key",
+               "Tuple method: count. the number of times a value is in a tuple. tuple.count('happy')",
+               "). Index: where a value is found inside the tuple",
+               "tuples can be nested and you can also slice them",
+               "sets don't have order and no duplicate values. No index because no order {} are used. there is also a set function like dict function",
+               "set methods: add t.add(4) only works if the value isn't there yet. Remove. t.remove(2) 2 is removed.",
+               "set method: discard is you don't want to worry about errors if something isn't there. It removes something",
+               "set method: copy. copies. Clear: removes everything.",
+               "set math: | group two sets together. & look at those who are in both sets",
 
                ]
 
