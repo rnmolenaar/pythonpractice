a2aa2f6df4705ed30ffdb903f92ee06e38b14c5c
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2023-11-27 23:58:09 +0100

    Update pythonfacts.py

--- pythonfacts.py
+++ pythonfacts.py
@@ -137,6 +137,7 @@ python_info = ["With random.choice(listname), you can pick a random item from a 
                "There is a Wikipedia module',
                "sorted() reverse= True to do it backwards, key=lambda to sort by a certain keyword then add another word to sort by key username:  ",
                "reversed function takes in an iterable. Different from reverse method in list. This can be used with strings",
+               "len can also be used with a dictionary based on the amount of key value pairs",
   
 
    
