9092c767982f5816a54000d5f88d0527755d8da3
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2023-11-29 22:40:18 +0100

    Update errors.py

--- errors.py
+++ errors.py
@@ -2,3 +2,16 @@
 
 if type(arg) is not str:
   raise TypeError("this must be an instance of str")
+
+
+
+
+def get dictionary, key):
+  try:
+    return dictionary[key]
+  except: KeyError:
+    return None
+
+dictionary = {"Country": "Netherlands"]
+
+print(get(dictionary, "name"))
