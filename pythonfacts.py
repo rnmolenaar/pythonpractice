2cd2f4ca5ec9fe18c5d3d09e6bd93fe34b108fba
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2023-12-01 23:50:54 +0100

    Update pythonfacts.py

--- pythonfacts.py
+++ pythonfacts.py
@@ -175,7 +175,7 @@ python_info = ["With random.choice(listname), you can pick a random item from a 
 " from random import randint, choice, shuffle to only import the methods you need. Then you don't need to do random.choice, but just choice",
 "If you create functions on a seperate file, you can just call the specific functions when importing from file import function",
 "Make a module if you want to use code in more than one place. If your file is very" 
-
+"if you want to import a module from another file in the same directory, filename.functioname()",
 
 
 
