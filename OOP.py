aeea4307a255144890987c6fe71ad26a8a987e9b
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2023-12-05 21:06:19 +0100

    Update OOP.py

--- OOP.py
+++ OOP.py
@@ -8,5 +8,11 @@ dog = Animal()
 rat = Animal()
 print(type(dog))
 
+# make a class of comments online
 
+class Comment():
+    def __init__(self, user, text, likes=0):
+        self.user = user
+        self.text = text
+        self.likes = likes
 
