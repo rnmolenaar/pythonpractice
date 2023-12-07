602d72d31cd38c06f23ad01a9a7226b7b1624504
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2023-12-07 19:20:53 +0100

    Update pythonfacts.py

--- pythonfacts.py
+++ pythonfacts.py
@@ -197,7 +197,8 @@ python_info = ["With random.choice(listname), you can pick a random item from a 
 "use CamelCase when defining classes. And singular", 
 "Classes can have a __init__ method. This gets called every time you make an object of the class", 
  "self refers to the objects, whatever comes after that can be put in: self.name =", 
-"
+"Private methods, attributes do not exist in Python", 
+               
 
 
 
