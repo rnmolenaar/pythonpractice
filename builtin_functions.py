d8611472f52316285d2d00d051aad2f5efd49b6a
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2023-12-15 00:00:09 +0100

    Update builtin_functions.py

--- builtin_functions.py
+++ builtin_functions.py
@@ -3,3 +3,21 @@
 snake = "python"
 
 print(list(snake))
+
+# bool
+
+check_bool = 0
+
+print(bool(check_bool))
+
+# input
+
+name = input("What is your name?: ")
+
+print("Hello " + name)
+
+# input + int/float
+
+age = int(input("What is your age?: "))
+
+print("You are " +str(age) + " years old")
