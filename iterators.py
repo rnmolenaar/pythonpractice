121090fe9fcefc036a6e5f097d12fda2330f8f2a
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2023-12-14 18:06:41 +0100

    Update iterators.py

--- iterators.py
+++ iterators.py
@@ -1,1 +1,19 @@
 
+def year():
+  months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ]
+
+  for month in months: 
+    yield month
