d1ab1c9862aa6afb683402c40ac0042bcb981ac9
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2023-12-15 22:47:00 +0100

    Create information.py

---
+++ information.py
@@ -0,0 +1,14 @@
+# data types in Python
+
+text_type = ["str"]
+
+numeric_type = ["int", "float", "complex"]
+
+mapping_type = ["dict"]
+
+sequence_type = ["list", "tuple", "range"]
+
+set_type = ["set", "frozenset"]
+
+boolean_type = ["bool"]
+
