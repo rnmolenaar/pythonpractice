963ebb5b27393063ebe2ff5ea034c126d1dcccbb
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2023-12-27 12:23:09 +0100

    Update strings.py

--- strings.py
+++ strings.py
@@ -3,3 +3,9 @@
 name = "Napoleon Bonaparte"
 slicing_names = name[0:12:4]
 print(slicing_names)
+
+#slicing if you only want the letters up until a certain index
+
+name = "Napoleon Bonaparte"
+slicing_names = name[:11]
+print(slicing_names)
