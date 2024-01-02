f8eaba65154e9a5ca4cd6bbb68f877cf135d0d37
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2024-01-02 17:21:52 +0100

    Update builtin_functions.py

--- builtin_functions.py
+++ builtin_functions.py
@@ -37,6 +37,10 @@ age = int(input("What is your age?: "))
 
 print("You are " +str(age) + " years old")
 
+#int() converts a float into an int by chopping of everything behind the decimal. It doesn't round up
+
+change_to_integer = int(34.7262) # 34
+
 
 # Len + input
 
