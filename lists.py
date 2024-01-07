1157a44ff9a5ffeacd27db897a6bf317f071ebd8
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2024-01-07 13:22:09 +0100

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,3 +1,5 @@
+#Lists start counting at 0, just like ranges
+
 # return index of a list
 
 def index_list(arr, idx):
@@ -6,3 +8,21 @@ def index_list(arr, idx):
     return arr[idx + len(arr)]
 
 print(index_list([1,2,3,4,5], -2))
+
+# check if something is in a list (you will get True or False
+
+my_list = [0.5, "what", "nono", ["blabla", "yay"]]
+
+print("yelp" in my_list)
+
+######### build in functions
+#len()
+
+#list() to create a list out of a range
+
+pet = list(range(1,200))
+
+print(pet)
+
+##################built in methods for lists
+
