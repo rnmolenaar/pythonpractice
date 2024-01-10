2dad0e1179d674c884a95313fee6ba10f1eb9a95
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2024-01-10 16:38:05 +0100

    Update tuplessets.py

--- tuplessets.py
+++ tuplessets.py
@@ -1,1 +1,6 @@
 # tuple immutable and ordered. Faster than lists. Safer from bugs. Tuples can be used as keys in dict. 
+# some dict methods return tuples in a list, like .items()
+earth = { "life": True, "inner planet": True, "moon": 1, "size_ranked": 5}
+
+planet = earth.items()
+print(planet)
