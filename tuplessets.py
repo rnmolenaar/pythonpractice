e93fcf3e3aaaccd10eeb6804da24e171624ee9f1
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2024-01-10 16:43:13 +0100

    Update tuplessets.py

--- tuplessets.py
+++ tuplessets.py
@@ -1,6 +1,16 @@
 # tuple immutable and ordered. Faster than lists. Safer from bugs. Tuples can be used as keys in dict. 
+# can use slices
+
 # some dict methods return tuples in a list, like .items()
 earth = { "life": True, "inner planet": True, "moon": 1, "size_ranked": 5}
 
 planet = earth.items()
 print(planet)
+
+# tuple methods
+
+# .count() how many times something is in a tuple
+
+tuple_name.count(name)
+
+#.index() what is the index of (something). Only shows the first occassion. 
