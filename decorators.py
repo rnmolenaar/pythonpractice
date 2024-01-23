a35f5b5994a83efe6c1b90abb09e9cd393fc9b49
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2024-01-23 23:49:24 +0100

    Update decorators.py

--- decorators.py
+++ decorators.py
@@ -1,1 +1,1 @@
-# allows your to modify the behavior of functions. Then you don' have to change the code
+# allows your to modify the behavior of functions. Then you don' have to change the code. It is a function that takes in another function and enhances it
