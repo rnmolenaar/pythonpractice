89cec20c99a8c9ec33fb659ebd77bd6c700c8b28
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2024-01-25 00:04:39 +0100

    Update dunder.py

--- dunder.py
+++ dunder.py
@@ -1,1 +1,9 @@
 # dunder methods start and end with __. Also called magic methods. 
+
+
+# __name__ Every python file has a  __name__ variable. The main file being run has the value __main__
+
+def show_name():
+  print(f"the name is {__name__}")
+
+show_name()
