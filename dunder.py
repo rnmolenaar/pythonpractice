c0b8655636fe99a6ccf7a856b12832d8d1e7cee8
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2024-01-25 23:42:47 +0100

    Update dunder.py

--- dunder.py
+++ dunder.py
@@ -7,3 +7,8 @@ def show_name():
   print(f"the name is {__name__}")
 
 show_name()
+
+# __init__(self, ...) is used when an object is made from a class. Self is the object here. 
+
+
+# __new__ create a new object 
