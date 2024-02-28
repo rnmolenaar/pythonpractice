2f9e5450db91fc58dcf5022b4e93b99131683418
rnmolenaar <135530940+rnmolenaar@users.noreply.github.com>
2024-02-28 21:20:32 +0100

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -3,3 +3,13 @@
 # most common alias is pd. make sure to execute it
 
 import pandas as pd
+
+# Series is ordered. Works like dictionary but value doesn't need to be unique
+
+queens_regnant = ["Mary I", "Elizabeth I", "Mary II"]
+pd.Series(queens_regnant)
+# When executed, numbers will be assigned to each value. It starts with 0
+0         Mary I
+1    Elizabeth I
+2        Mary II
+
